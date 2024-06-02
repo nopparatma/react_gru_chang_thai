b3b209f7f19cdc70b84a2f12fb08957ff9b659c1
Nopparat Manoi <yachty22541@gmail.com>
2024-06-03 01:54:19 +0700

    edit home banner [desktop]

--- src/components/BannerHome.tsx
+++ src/components/BannerHome.tsx
@@ -1,42 +1,45 @@
-import { useTranslations } from 'next-intl';
-import Image from 'next/image';
-import mainBackground from '../../public/main_background.png';
-import imagePresenter from '../../public/image_presenter3.png';
+import { useTranslations } from "next-intl";
+import Image from "next/image";
+import mainBackground from "../../public/main_background.png";
+import imagePresenter from "../../public/image_presenter3.png";
 
 function BannerHome() {
-    const t = useTranslations('metadata');
+  const t = useTranslations("metadata");
 
-    return (
-        <>
-            <div className="sm:h-screen h-[760px]">
-                <Image
-                    className='brightness-50 object-cover sm:h-screen h-[760px] absolute z-0'
-                    alt="main_background"
-                    src={mainBackground}
-                />
-                <div className='relative flex flex-col pt-[80px] sm:flex-row h-full justify-between mx-8 z-10'>
-                    <div className='flex-none flex flex-col text-center sm:text-left sm:justify-center'>
-                        <p className="text-gold-gradient text-[32px] sm:text-[52px]">GruChangThai Antique</p>
-                        <p className="text-gold-gradient text-[32px] sm:text-[52px]">Gold Jewelry</p>
-                        <p className='text-white mt-4 text-[20px]'>The Perfect Jewels for you</p>
-                        <div className='mt-8'>
-                            <button className='button-gold-gradient'>
-                                Explore More
-                            </button>
-                        </div>
-                        <p className='text-white sm:mt-32 mt-8 text-[20px]'>Call: 084-8047253</p>
-                    </div>
-                    <div className='flex flex-col justify-end'>
-                        <Image
-                            className='object-cover'
-                            alt="image_presenter"
-                            src={imagePresenter}
-                        />
-                    </div>
-                </div>
+  return (
+    <>
+      <div className="h-screen">
+        <Image
+          className="brightness-50 object-cover h-full absolute z-0"
+          alt="main_background"
+          src={mainBackground}
+        />
+        <div className="relative flex h-full mx-8 z-10">
+          <div className="text-left my-auto">
+            <p className="text-gold-gradient text-6xl py-4">
+              GruChangThai Antique
+            </p>
+            <p className="text-gold-gradient text-6xl py-4">Gold Jewelry</p>
+            <p className="text-white mt-4 text-xl">
+              The Perfect Jewels for you
+            </p>
+            <div className="mt-8">
+              <button className="button-gold-gradient text-xl">
+                Explore More
+              </button>
             </div>
-        </>
-    )
+            <p className="text-white mt-32 text-xl">Call: 084-8047253</p>
+          </div>
+        </div>
+        <div className="">
+          <Image
+            className="object-contain z-20 absolute bottom-0 right-8 h-full w-auto"
+            alt="image_presenter"
+            src={imagePresenter}
+          />
+        </div>
+      </div>
+    </>
+  );
 }
-
-export default BannerHome
+export default BannerHome;
