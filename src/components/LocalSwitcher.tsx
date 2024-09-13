12a80bbd831e1bbf2d7c9edfbcecb8b1af3cd114
Nopparat Manoi <yachty22541@gmail.com>
2024-09-13 21:35:32 +0700

    edit localSwitcher

--- src/components/LocalSwitcher.tsx
+++ src/components/LocalSwitcher.tsx
@@ -1,26 +1,31 @@
-"use client"
+"use client";
 
-import { useRouter, usePathname } from '@/navigation';
-import { useLocale } from 'next-intl';
-import React, { ChangeEvent } from 'react'
+import { useRouter, usePathname } from "@/navigation";
+import { useLocale } from "next-intl";
+import React, { ChangeEvent } from "react";
+
+type Locale = "en" | "th";
 
 function LocalSwitcher() {
+  const router = useRouter();
+  const pathname = usePathname();
+  const locale = useLocale();
 
-    const router = useRouter();
-    const pathname = usePathname();
-    const locale = useLocale();
+  const onChangeLang = (e: ChangeEvent<HTMLSelectElement>) => {
+    const nextLocale = e.target.value as Locale;
+    router.replace(pathname, { locale: nextLocale });
+  };
 
-    const onChangeLang = (e: ChangeEvent<HTMLSelectElement>) => {
-        const nextLocale = e.target.value;
-        router.replace(pathname, { locale: nextLocale });
-    }
-
-    return (
-        <select className='bg-transparent' defaultValue={locale} onChange={onChangeLang}>
-            <option value='en'>English</option>
-            <option value='th'>Thai</option>
-        </select>
-    )
+  return (
+    <select
+      className="bg-transparent"
+      defaultValue={locale}
+      onChange={onChangeLang}
+    >
+      <option value="en">English</option>
+      <option value="th">Thai</option>
+    </select>
+  );
 }
 
-export default LocalSwitcher
+export default LocalSwitcher;
