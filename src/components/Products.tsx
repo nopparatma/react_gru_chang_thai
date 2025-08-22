8b6c98ce56ff5a43e0dc96a10c18c498def12a00
Nopparat Manoi <yachty22541@gmail.com>
2025-08-22 17:12:40 +0700

    fix responsive

--- src/components/Products.tsx
+++ src/components/Products.tsx
@@ -64,7 +64,7 @@ export default function Products() {
       </div>
 
       <div
-        className="w-full px-10"
+        className="w-full md:px-10"
         onMouseEnter={() => setIsHovered(true)}
         onMouseLeave={() => setIsHovered(false)}
       >
